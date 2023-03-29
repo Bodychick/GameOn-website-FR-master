a8938ebbcfa6fff6226b54ab8b45ebf455e3460e
Alexandre Penneroux <101572526+Bodychick@users.noreply.github.com>
2023-03-29 08:27:56 +0200

    Amélioration algo

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,58 +12,31 @@ const closeButton = document.querySelector(".close");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const radio = document.querySelectorAll(".checkbox-input");
+const radios = document.querySelectorAll(".checkbox-input");
+const formDataRadio = document.getElementById("formDataRadio");
 
 // Récupération de l'élément <p> ayant la classe "text-label"
-const textLabel = document.querySelector("p.text-label");
-const warningMessage = document.querySelector("p.warning-message");
+//const textLabel = document.querySelector("p.text-label");
+//const warningMessage = document.querySelector("p.warning-message");
 
 // Récupération des inputs
 const firstName= document.getElementById('first');
 const lastName= document.getElementById('last');
 const emailInput = document.getElementById("email");
-const email = emailInput.value;
 
-//Listener sur les inputs firstname et lastname
-firstName.addEventListener('focusout',function checkTheName() {
-  checkName(firstName,"firstNameID");
-});
+const birthDate = document.getElementById("birthdate");
+var today = new Date();
+const annee = today.getFullYear();
+const mois = String(today.getMonth() + 1).padStart(2, '0');
+const jour = String(today.getDate()).padStart(2, '0');
+const todaysDate = `${annee}-${mois}-${jour}`;
+birthDate.max = todaysDate;
 
-lastName.addEventListener('focusout',function checkTheName() {
-  checkName(lastName,"lastNameID");
-});
-
-
-/* CHECK DE L'ADRESSE MAIL AVEC AFFICHAGE D'UN MESSAGE SI PROBLEME */
-emailInput.addEventListener("focusout",function testEmail(email){
-  console.log(validateEmail(email));
-  if (validateEmail(email) && document.getElementById("checkMail")!=null) {
-    // L'email est valide, continuer avec le traitement du formulaire
-    const element= document.getElementById("checkMail");
-    element.parentElement.removeChild(element);
-  } 
-  else if (validateEmail(email) == false && document.getElementById("checkMail")==null) {
-    // L'email n'est pas valide, afficher un message d'erreur
-    createElement("p","checkMail",emailInput,"Veuillez entrer un email valide");
-  }
-})
-
-/* Vérifier qu'un nom et un prénom sont renseignés */
-function checkName($name,$id) {
-  if ($name.value.length<=2 && document.getElementById($id) == null )
-  {
-    createElement("p",$id,$name,"Veuillez entrer votre prénom");
-  }
-  else if($name.value.length>2 && document.getElementById($id) != null) {
-    const element= document.getElementById($id);
-    element.parentElement.removeChild(element);
-  }
-
-}
+//Nombre d'évènements déjà participé
+const quantity = document.getElementById("quantity");
 
 //Check si submit
 const submitButton = document.querySelector(".btn-submit");
-
 
 // close modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -77,28 +50,113 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Listener sur les inputs firstname et lastname
+firstName.addEventListener('focusout',function checkTheName() {
+  checkName(firstName,"firstNameID");
+});
+
+lastName.addEventListener('focusout',function checkTheName() {
+  checkName(lastName,"lastNameID");
+});
+
+
+/* CHECK DE L'ADRESSE MAIL AVEC AFFICHAGE D'UN MESSAGE SI PROBLEME */
+emailInput.addEventListener("focusout",function testEmail(){
+  var email = emailInput.value;
+  if (isValidateEmail(email) && document.getElementById("checkMail")!=null) {
+    // L'email est valide, continuer avec le traitement du formulaire et il y a déjà un message affiché
+    const element= document.getElementById("checkMail");
+    element.parentElement.removeChild(element);
+  } 
+  else if (isValidateEmail(email) == false && document.getElementById("checkMail")==null) {
+    // L'email n'est pas valide, afficher un message d'erreur et il n'y a aucun message d'affiché
+    createElement("p","checkMail",emailInput,"Veuillez entrer un email valide");
+  }
+})
+
+/* CHECK SI DATE DE NAISSANCE */
+birthDate.addEventListener("focusout", function checkBirthDate(){
+  const dateOfBirth=birthDate.value;
+  if(dateOfBirth >= todaysDate && document.getElementById("birthdateID")== null){
+    //On ajoute un message s'il y en a pas de mis encore
+    createElement("p","birthdateID",birthDate,"Veuillez entrer une date antérieure à celle d'aujourd'hui");
+    console.log("Pas de message + Date sup ou égale à today")
+  }
+  else if(dateOfBirth >= todaysDate && document.getElementById("birthdateID")!= null){
+    //DO NOTHING parce que il y a déjà un message d'affiché
+    console.log("On laisse tout si message et date tjs today ou sup")
+  }
+  else if(dateOfBirth == "" && document.getElementById("birthdateID") == null){
+    //On retire le message dans les autres cas
+    createElement("p","birthdateID",birthDate,"Veuillez entrer votre date de naissance");
+    console.log("On message si date non rentré et pas de messages")
+  }
+  else if(dateOfBirth < todaysDate && document.getElementById("birthdateID") != null) {
+    console.log("Si date est bonne et un message on enlève")
+    const element= document.getElementById("birthdateID");
+    element.parentElement.removeChild(element);
+  }
+
+});
+
+quantity.addEventListener("focusout",function checkQuantity(){
+  const quantityVal = quantity.value;
+  console.log(quantity.value)
+  if(quantityVal=="" && document.getElementById("quantityID")==null) {
+    createElement("p","quantityID",quantity,"Veuillez sélectionner une option");
+  } 
+  else if (quantityVal=="" && document.getElementById("quantityID")!=null)
+  {
+    //ON LAISSE COMME C'EST
+  }
+  else if (quantityVal>=0 && document.getElementById("quantityID")!=null)
+  {
+    const element= document.getElementById("quantityID");
+    element.parentElement.removeChild(element);
+  }
+});
+
+/* Vérifier qu'un nom et un prénom sont renseignés */
+function checkName($name,$id) {
+  if ($name.value.length<=2 && document.getElementById($id) == null )
+  {
+    createElement("p",$id,$name,"2 caractères minimum sont nécessaires pour ce champ");
+  }
+  else if($name.value.length>2 && document.getElementById($id) != null) {
+    const element= document.getElementById($id);
+    element.parentElement.removeChild(element);
+  }
+
+}
+
 //CHECK SI Radio est coché
-modalbg.addEventListener("show.bs.modal", function(){
-  console.log("Modal chargée");
-  checkRadio();
+modalbg.addEventListener("mouseenter", function checkAllRadio(){
+  let isChecked = checkRadio();
+  console.log(isChecked)  
+  if (isChecked == false && document.getElementById("formDataRadio")==null){
+    createElement("p","formDataRadio",formDataRadio,"Veuillez sélectionner un lieu")
+  }
+  else if (isChecked == true && document.getElementById("formDataRadio")!=null){
+    const element= document.getElementById("formDataRadio");
+    element.parentElement.removeChild(element);
+  }
 });
 
 function checkRadio() {
-  var nbRadioChecked=0;
-  radio.forEach((item)=> {
+  let isRadioChecked=false;
+  radios.forEach((item)=> {
     if(item.checked){
-      nbRadioChecked++;
+      isRadioChecked=true;
     }
   });
-  if(nbRadioChecked==0 && warningMessage){
-    createElement();
-  }
+  return isRadioChecked;
 }
 
 
+//Fonction pour créer un message d'erreur 
 function createElement($typeElem="p",$id="",$element,$message="Veuillez saisir une information"){
   // Création de l'élément à insérer
-  console.log("je sui en create element")
+  //console.log("je suis en create element")
   const errorMessage = document.createElement($typeElem);
   errorMessage.setAttribute("id",$id);
   errorMessage.textContent = $message;
@@ -108,19 +166,18 @@ function createElement($typeElem="p",$id="",$element,$message="Veuillez saisir u
   $element.insertAdjacentElement("afterend", errorMessage);
 }
 
-function validateEmail(email) {
+function isValidateEmail(email) {
   // Expression régulière pour valider un email
-  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,}$/;
-  
+  const regex =/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,}$/;
   // Vérifier si l'email est valide en utilisant test() de RegExp
   return regex.test(email);
 }
 
 
 //Alert quand bouton submit cliqué
-submitButton.addEventListener("click",function(){
+submitButton.addEventListener("submit",function(){
   console.log('Click sur bouton');
-  windows.alert('Formulaire envoyé');
+  window.alert('Formulaire envoyé avec succès');
 });
 
 
